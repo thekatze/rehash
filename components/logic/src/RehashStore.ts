814c296820ea9c012ca9886e48cdb55f277833d1
Leo Katzengruber <leo.katzengruber@consolidate.at>
2022-01-15 19:42:24 +0100

    add editorconfig

--- components/logic/src/RehashStore.ts
+++ components/logic/src/RehashStore.ts
@@ -1,3 +1,25 @@
 console.log(crypto.randomUUID());
 
 console.log(new URL("https://app.rehash.one/123").host);
+
+import { GeneratorEntry } from "./RehashGenerator";
+import localforage from "localforage";
+
+export interface StoreEntry extends GeneratorEntry {
+  id: number;
+  displayName?: string;
+}
+
+export class RehashStore {
+  private store: LocalForage;
+
+  constructor(storeName: string) {
+    this.store = localforage.createInstance({
+      name: storeName,
+    });
+  }
+
+  public unlock(): boolean {
+    return false;
+  }
+}
--- components/logic/src/StoreEntry.ts
+++
@@ -1,6 +0,0 @@
-import { GeneratorEntry } from "./RehashGenerator";
-
-export interface StoreEntry extends GeneratorEntry {
-  id: number;
-  displayName?: string;
-}
