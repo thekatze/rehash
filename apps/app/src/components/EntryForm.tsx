05efe42fdfefcdc96ad2d17ab77b9bc9bb7d2871
Leo Katzengruber <leofel.katz@gmail.com>
2022-07-17 13:41:00 +0100

    git commit -am doesnt add new files, huh

---
+++ apps/app/src/components/EntryForm.tsx
@@ -0,0 +1,97 @@
+import { useI18n } from "@/i18n/I18nProvider";
+import { FormControl, FormLabel, Input, Textarea, VStack } from "@hope-ui/solid";
+import { StoreEntry } from "@rehash/logic";
+import { Component } from "solid-js";
+import { SetStoreFunction } from "solid-js/store";
+
+
+interface EntryFormProps {
+  entry: Partial<StoreEntry>;
+  setEntry: SetStoreFunction<Partial<StoreEntry>>;
+}
+
+const EntryForm: Component<EntryFormProps> = ({ entry, setEntry }) => {
+  const [t] = useI18n();
+
+  return (
+    <VStack spacing="$4" alignItems="stretch">
+      <FormControl>
+        <FormLabel for="url">{t("URL")}</FormLabel>
+        <Input
+          id="url"
+          value={entry.url}
+          onInput={(e: any) => setEntry("url", () => e.currentTarget.value)}
+        />
+      </FormControl>
+      <FormControl>
+        <FormLabel for="username">{t("USERNAME")}</FormLabel>
+        <Input
+          id="username"
+          value={entry.username}
+          onInput={(e: any) =>
+            setEntry("username", () => e.currentTarget.value)
+          }
+        />
+      </FormControl>
+      <FormControl>
+        <FormLabel for="displayName">{t("DISPLAY_NAME")}</FormLabel>
+        <Input
+          id="displayName"
+          value={entry.displayName}
+          onInput={(e: any) =>
+            setEntry("displayName", () => e.currentTarget.value)
+          }
+        />
+      </FormControl>
+      <FormControl>
+        <FormLabel for="notes">{t("NOTES")}</FormLabel>
+        <Textarea
+          id="notes"
+          value={entry.notes}
+          onInput={(e: any) =>
+            setEntry("notes", () => e.currentTarget.value)
+          }
+        />
+      </FormControl>
+      <FormControl>
+        <FormLabel for="iteration">{t("ITERATION")}</FormLabel>
+        <Input
+          id="iteration"
+          value={entry.options?.iteration ?? 1}
+          type="number"
+          onInput={(e: any) =>
+            setEntry("options", "iteration", () => {
+              const newValue = parseInt(e.currentTarget.value);
+              if (Number.isNaN(newValue)) {
+                return 1;
+              }
+
+              return newValue;
+            })
+          }
+        />
+      </FormControl>
+      <FormControl>
+        <FormLabel for="length">{t("LENGTH")}</FormLabel>
+        <Input
+          id="length"
+          value={entry.options?.length ?? 32}
+          type="number"
+          onInput={(e: any) =>
+            setEntry("options", "length", () => {
+              const newValue = parseInt(e.currentTarget.value);
+              if (Number.isNaN(newValue)) {
+                return 8;
+              }
+
+              return Math.max(newValue, 8);
+            })
+          }
+        />
+      </FormControl>
+    </VStack>
+  );
+
+}
+
+export default EntryForm;
---
+++ apps/app/src/components/GeneratorOptionsForm.tsx
@@ -0,0 +1,78 @@
+import { useI18n } from "@/i18n/I18nProvider";
+import { VStack, FormControl, FormLabel, Input } from "@hope-ui/solid";
+import { GeneratorOptions } from "@rehash/logic";
+import { Component } from "solid-js";
+import { SetStoreFunction } from "solid-js/store";
+
+interface GeneratorOptionsForm {
+  generatorOptions: GeneratorOptions;
+  setGeneratorOptions: SetStoreFunction<GeneratorOptions>;
+}
+
+const GeneratorOptionsForm: Component<GeneratorOptionsForm> = ({
+  generatorOptions,
+  setGeneratorOptions,
+}) => {
+  const [t] = useI18n();
+
+  return (
+    <VStack>
+      <FormControl>
+        <FormLabel for="iterations">{t("ITERATIONS")}</FormLabel>
+        <Input
+          id="iterations"
+          value={generatorOptions.iterations}
+          type="number"
+          onInput={(e: any) =>
+            setGeneratorOptions("iterations", () => {
+              const newValue = parseInt(e.currentTarget.value);
+              if (Number.isNaN(newValue)) {
+                return 1;
+              }
+
+              return Math.max(newValue, 1);
+            })
+          }
+        />
+      </FormControl>
+      <FormControl>
+        <FormLabel for="parallelism">{t("PARALLELISM")}</FormLabel>
+        <Input
+          id="parallelism"
+          value={generatorOptions.parallelism}
+          type="number"
+          onInput={(e: any) =>
+            setGeneratorOptions("parallelism", () => {
+              const newValue = parseInt(e.currentTarget.value);
+              if (Number.isNaN(newValue)) {
+                return 1;
+              }
+
+              return Math.max(newValue, 1);
+            })
+          }
+        />
+      </FormControl>
+      <FormControl>
+        <FormLabel for="memorySize">{t("MEMORY_SIZE")}</FormLabel>
+        <Input
+          id="memorySize"
+          value={generatorOptions.memorySize}
+          type="number"
+          onInput={(e: any) =>
+            setGeneratorOptions("memorySize", () => {
+              const newValue = parseInt(e.currentTarget.value);
+              if (Number.isNaN(newValue)) {
+                return 128;
+              }
+
+              return Math.max(newValue, 128);
+            })
+          }
+        />
+      </FormControl>
+    </VStack>
+  );
+};
+
+export default GeneratorOptionsForm;
