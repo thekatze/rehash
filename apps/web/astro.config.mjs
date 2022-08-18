a9f4b0ca76f18d4b549041e758a7c5285d4e6782
TheKatze <leofel.katz@gmail.com>
2022-08-18 21:36:27 +0200

    optimize css

--- apps/web/astro.config.mjs
+++ apps/web/astro.config.mjs
@@ -1,10 +1,19 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import solidJs from "@astrojs/solid-js";
 import tailwind from "@astrojs/tailwind";
+import cssnano from "cssnano";
 
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [solidJs(), tailwind(), sitemap()]
+  site: "https://www.rehash.one",
+  integrations: [solidJs(), tailwind(), sitemap()],
+  vite: {
+    css: {
+      postcss: {
+        plugins: [cssnano({ preset: "advanced" })],
+      },
+    },
+  },
 });
