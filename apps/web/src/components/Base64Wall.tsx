aa922265bb72a40a81914084d5161eb89a4aef01
Leo Katzengruber <leofel.katz@gmail.com>
2022-08-27 23:16:48 +0200

    replace placeholder content

--- apps/web/src/components/Base64Wall.tsx
+++ apps/web/src/components/Base64Wall.tsx
@@ -5,8 +5,8 @@ const randomString = (length: number) =>
     .map(() => Math.random().toString().substring(2))
     .join("");
 
-const Base64Wall: Component = () => {
-  const [data] = createSignal<string>(randomString(150));
+const Base64Wall: Component<{ length?: number }> = ({ length = 175 }) => {
+  const [data] = createSignal<string>(randomString(length));
   const wall = () => btoa(data());
 
   return <>{wall}</>;
